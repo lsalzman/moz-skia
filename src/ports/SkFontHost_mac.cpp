4c6161523ce73b38e993965a8b34c62bc605c842
Lee Salzman <lsalzman@gmail.com>
2016-04-29 21:10:50 -0400

    Bug 1267180 - Don't draw emojis as paths when they are too big.
    Bug 1269247 - check that SkPaint has a typeface before using it

--- src/ports/SkFontHost_mac.cpp
+++ src/ports/SkFontHost_mac.cpp
@@ -723,6 +723,7 @@ public:
     SkUniqueCFRef<CTFontRef> fFontRef;
     SkUniqueCFRef<CFTypeRef> fOriginatingCFTypeRef;
     const bool fHasColorGlyphs;
+    bool hasColorGlyphs() const override { return fHasColorGlyphs; }
 
 protected:
     int onGetUPEM() const override;
