d88177875faf0b83ef9b6f66db8528c2f128bf54
Lee Salzman <lsalzman@gmail.com>
2016-10-07 16:24:35 -0400

    Bug 1271356 - cut out some utils and effects cruft we don't use in Skia builds

--- src/ports/SkGlobalInitialization_default.cpp
+++ src/ports/SkGlobalInitialization_default.cpp
@@ -5,47 +5,11 @@
  * found in the LICENSE file.
  */
 
-#include "Sk1DPathEffect.h"
-#include "Sk2DPathEffect.h"
-#include "SkAlphaThresholdFilter.h"
-#include "../../src/effects/SkArithmeticModePriv.h"
-#include "SkArcToPathEffect.h"
 #include "SkBitmapSourceDeserializer.h"
-#include "SkBlurImageFilter.h"
-#include "SkBlurMaskFilter.h"
-#include "SkColorFilterImageFilter.h"
-#include "SkColorMatrixFilterRowMajor255.h"
-#include "SkComposeImageFilter.h"
-#include "SkCornerPathEffect.h"
 #include "SkDashPathEffect.h"
-#include "SkDiscretePathEffect.h"
-#include "SkDisplacementMapEffect.h"
-#include "SkDropShadowImageFilter.h"
-#include "../../src/effects/SkEmbossMaskFilter.h"
-#include "../../src/effects/SkGaussianEdgeShader.h"
 #include "SkGradientShader.h"
-#include "SkHighContrastFilter.h"
 #include "SkImageSource.h"
-#include "SkLayerDrawLooper.h"
 #include "SkLayerRasterizer.h"
-#include "SkLightingImageFilter.h"
-#include "SkLightingShader.h"
-#include "SkLocalMatrixImageFilter.h"
-#include "SkLumaColorFilter.h"
-#include "SkMagnifierImageFilter.h"
-#include "SkMatrixConvolutionImageFilter.h"
-#include "SkMergeImageFilter.h"
-#include "SkMorphologyImageFilter.h"
-#include "SkNormalSource.h"
-#include "SkOffsetImageFilter.h"
-#include "../../src/effects/SkOverdrawColorFilter.h"
-#include "SkPaintImageFilter.h"
-#include "SkPerlinNoiseShader.h"
-#include "SkPictureImageFilter.h"
-#include "SkRRectsGaussianEdgeMaskFilter.h"
-#include "SkTableColorFilter.h"
-#include "SkTileImageFilter.h"
-#include "SkXfermodeImageFilter.h"
 
 // Security note:
 //
@@ -67,60 +31,15 @@
 void SkFlattenable::PrivateInitializer::InitEffects() {
     SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkBitmapSourceDeserializer)
 
-    // MaskFilter
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkEmbossMaskFilter)
-    SkBlurMaskFilter::InitializeFlattenables();
-    SkRRectsGaussianEdgeMaskFilter::InitializeFlattenables();
-
-    // DrawLooper
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkLayerDrawLooper)
-
     // Rasterizer
     SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkLayerRasterizer)
 
-    // ColorFilter
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkColorMatrixFilterRowMajor255)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkLumaColorFilter)
-    SkAlphaThresholdFilter::InitializeFlattenables();
-    SkArithmeticMode::InitializeFlattenables();
-    SkTableColorFilter::InitializeFlattenables();
-    SkOverdrawColorFilter::InitializeFlattenables();
-    SkHighContrastFilter::InitializeFlattenables();
-
     // Shader
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkPerlinNoiseShader)
     SkGradientShader::InitializeFlattenables();
-    SkLightingShader::InitializeFlattenables();
-    SkNormalSource::InitializeFlattenables();
-    SkGaussianEdgeShader::InitializeFlattenables();
 
     // PathEffect
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkArcToPathEffect)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkCornerPathEffect)
     SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkDashPathEffect)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkDiscretePathEffect)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkPath1DPathEffect)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkLine2DPathEffect)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkPath2DPathEffect)
 
     // ImageFilter
     SkImageFilter::InitializeFlattenables();
-    SkArithmeticImageFilter::InitializeFlattenables();
-    SkXfermodeImageFilter::InitializeFlattenables();
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkDilateImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkDisplacementMapEffect)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkDropShadowImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkErodeImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkImageSource)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkLocalMatrixImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkPaintImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkPictureImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkTileImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkMagnifierImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkMatrixConvolutionImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkOffsetImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkComposeImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkMergeImageFilter)
-    SK_DEFINE_FLATTENABLE_REGISTRAR_ENTRY(SkColorFilterImageFilter)
-    SkLightingImageFilter::InitializeFlattenables();
 }
