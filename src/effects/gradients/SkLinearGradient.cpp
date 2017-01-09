f51cc7c1b0d995c0d92937aecdfee685395170ae
Lee Salzman <lsalzman@gmail.com>
2017-01-09 11:10:09 -0500

    Bug 1316422 - fix clamping of vertical gradient lerp in Skia

--- src/effects/gradients/SkLinearGradient.cpp
+++ src/effects/gradients/SkLinearGradient.cpp
@@ -189,7 +189,12 @@ void shadeSpan_linear_vertical_lerp(TileProc proc, SkGradFixed dx, SkGradFixed f
     // We're a vertical gradient, so no change in a span.
     // If colors change sharply across the gradient, dithering is
     // insufficient (it subsamples the color space) and we need to lerp.
-    unsigned fullIndex = proc(SkGradFixedToFixed(fx) - (SK_FixedHalf >> SkGradientShaderBase::kCache32Bits));
+    unsigned fullIndex = proc(SkGradFixedToFixed(fx));
+    if (fullIndex >= (SK_FixedHalf >> SkGradientShaderBase::kCache32Bits)) {
+        fullIndex -= SK_FixedHalf >> SkGradientShaderBase::kCache32Bits;
+    } else {
+        fullIndex = 0;
+    }
     unsigned fi = fullIndex >> SkGradientShaderBase::kCache32Shift;
     unsigned remainder = fullIndex & ((1 << SkGradientShaderBase::kCache32Shift) - 1);
 
