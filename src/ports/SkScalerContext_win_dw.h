8466c9c0675290bc0fcc3577ca87cadb6a8906d1
Lee Salzman <lsalzman@gmail.com>
2018-01-23 22:06:15 -0500

    Bug 842894 - Support DirectWrite using the Skia backend.
    Bug 1309917 - Stop defaulting to system wide default fonts if no IDWriteFont exists.
    
    Bug 1315568 - Use Force GDI information from SkTypeface for GDI rendering modes in skia
    
    Bug 1321901 - Use IDWriteFontFace::GetRecommendedRenderingMode for font rendering mode in Skia
    
    Bug 1323587 - Use correct gamma and contrast for dwrite fonts
    
    Bug 1322897 - Use only the G channel for grayscale AA Dwrite fonts and Skia
    
    fix access of dwrite typeface in scaler context
    
    Bug 1376026 - pass gamma and contract directly to DWrite typefaces

--- src/ports/SkScalerContext_win_dw.h
+++ src/ports/SkScalerContext_win_dw.h
@@ -70,6 +70,7 @@ private:
     DWRITE_MEASURING_MODE fMeasuringMode;
     DWRITE_TEXT_ANTIALIAS_MODE fAntiAliasMode;
     DWRITE_GRID_FIT_MODE fGridFitMode;
+    SkTScopedComPtr<IDWriteRenderingParams> fDefaultRenderingParams;
     bool fIsColorFont;
 };
 
