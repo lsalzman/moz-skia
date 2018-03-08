44dc943aa2cbd2184882a1143d928059a04aa5da
Lee Salzman <lsalzman@gmail.com>
2018-03-07 21:46:24 -0500

    support deprecated clip ops

--- include/config/SkUserConfig.h
+++ include/config/SkUserConfig.h
@@ -142,6 +142,8 @@
 
 #define SK_DISABLE_SLOW_DEBUG_VALIDATION 1
 
+#define SK_SUPPORT_DEPRECATED_CLIPOPS
+
 #ifndef MOZ_IMPLICIT
 #  ifdef MOZ_CLANG_PLUGIN
 #    define MOZ_IMPLICIT __attribute__((annotate("moz_implicit")))
