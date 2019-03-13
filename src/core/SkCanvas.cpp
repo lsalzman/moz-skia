b816c810eafe6e2f3eae9d421eedf68e71ef11c1
Lee Salzman <lsalzman@gmail.com>
2019-03-13 16:51:29 -0400

    restore SkCanvas::kPreserveLCDText_SaveLayerFlag

--- src/core/SkCanvas.cpp
+++ src/core/SkCanvas.cpp
@@ -1053,7 +1053,8 @@ void SkCanvas::internalSaveLayer(const SaveLayerRec& rec, SaveLayerStrategy stra
 
     sk_sp<SkBaseDevice> newDevice;
     {
-        const bool preserveLCDText = kOpaque_SkAlphaType == info.alphaType();
+        const bool preserveLCDText = kOpaque_SkAlphaType == info.alphaType() ||
+                                     (saveLayerFlags & kPreserveLCDText_SaveLayerFlag);
         const SkBaseDevice::TileUsage usage = SkBaseDevice::kNever_TileUsage;
         const bool trackCoverage =
                 SkToBool(saveLayerFlags & kMaskAgainstCoverage_EXPERIMENTAL_DONT_USE_SaveLayerFlag);
