55359814ae781a4f03e9583da505e51ea8995f34
Lee Salzman <lsalzman@gmail.com>
2023-04-01 15:11:25 -0400

    add custom font host for interoperating with Cairo fonts
    Bug 1269011 - silence SkCairoFTTypeface::onFilterRec warnings
    
    Bug 1274111 - use freetype directly for calculating glyph sizes in SkFontHost_cairo
    
    Bug 1287552 - revise SkFontHost_cairo to properly parse Fontconfig options.
    
    Bug 1287552 - only set FC_PIXEL_SIZE in pattern if not already supplied.
    
    Bug 1294207 - detect if FreeType supports lcd filtering in SkFontHost_cairo.
    
    Bug 1298691 - silence debug warning in SkScalerContext_CairoFT::generateFontMetrics
    
    Bug 1298833 - disable gamma correction in SkFontHost_cairo to match cairo-ft
    
    Bug 1298112 - Remove always-true null-check to fix compile warning
    
    fix SkFontHost_cairo for m55
    
    Bug 1308316 - ensure Skia disables hinting if Fontconfig disables it
    
    Bug 1318769 - make SkFontHost_cairo match cairo-ft's handling of FcPattern embeddedbitmap option
    
    Bug 1322337 - load color bitmaps in SkFontHost_cairo when building with FreeType before 2.5.0
    
    Bug 1350783 - support bitmaps fonts in gfxFcPlatformFontList
    
    fix SkFontHost_cairo for m59
    
    Bug 1400721 - fix Skia's glyph LCD filter padding for FreeType 2.8.1
    
    Bug 1471261 - Add global locking around FT_Load_Glyph to work around postscript hinter global data
    
    Bug 1478084 - fix recursive locking in SkFontHost_cairo
    
    Bug 1558375 - Don't set the FT_LOAD_NO_AUTOHINT flag for fonts that have FT_FACE_FLAG_TRICKY
    
    Bug 1547063 - Remove FcPattern usage from Skia
    
    Bug 1583707 - enable text subpixel positioning on Android and Linux
    
    Bug 1585584 - clear SharedFTFace lock owner when it is destroyed
    
    Bug 1495400 - don't draw non-scalable FT faces as paths
    
    Bug 1600470 - Reduce the emboldening strength used for synthetic-bold faces with FreeType
    
    Bug 1769279 - Disable FT_LOAD_COLOR when getting glyph metrics for fonts that include an SVG table

--- src/ports/SkFontHost_FreeType_common.cpp
+++ src/ports/SkFontHost_FreeType_common.cpp
@@ -29,16 +29,16 @@
 #include <vector>
 
 #include <ft2build.h>
-#include <freetype/freetype.h>
-#include <freetype/ftbitmap.h>
+#include FT_FREETYPE_H
+#include FT_BITMAP_H
 #ifdef FT_COLOR_H
-#   include <freetype/ftcolor.h>
+#   include FT_COLOR_H
 #endif
-#include <freetype/ftimage.h>
-#include <freetype/ftoutln.h>
-#include <freetype/ftsizes.h>
+#include FT_IMAGE_H
+#include FT_OUTLINE_H
+#include FT_SIZES_H
 // In the past, FT_GlyphSlot_Own_Bitmap was defined in this header file.
-#include <freetype/ftsynth.h>
+#include FT_SYNTHESIS_H
 
 using namespace skia_private;
 
@@ -47,7 +47,7 @@ namespace {
 }
 
 #if defined(FT_CONFIG_OPTION_SVG)
-#   include <freetype/otsvg.h>
+#   include FT_OTSVG_H
 #endif
 
 #ifdef TT_SUPPORT_COLRV1
@@ -78,6 +78,10 @@ namespace {
 #ifndef FT_LOAD_COLOR
 #    define FT_LOAD_COLOR ( 1L << 20 )
 #    define FT_PIXEL_MODE_BGRA 7
+#endif
+
+#ifndef FT_LOAD_BITMAP_METRICS_ONLY
+#    define FT_LOAD_BITMAP_METRICS_ONLY ( 1L << 22 )
 #endif
 
 #ifdef SK_DEBUG
