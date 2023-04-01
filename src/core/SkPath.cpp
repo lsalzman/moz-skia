d3e11649e4a83655d3570ae6aca5902dae5c59ee
Lee Salzman <lsalzman@gmail.com>
2023-04-01 14:54:19 -0400

    Bug 1255062 - disallow multiple moveTos in SkPath::isRectContour

--- src/core/SkPath.cpp
+++ src/core/SkPath.cpp
@@ -3679,6 +3679,9 @@ bool SkPathPriv::IsRectContour(const SkPath& path, bool allowPartial, int* currV
                     *currVerb -= 1;  // try move again afterwards
                     goto addMissingClose;
                 }
+                if (pts != *ptsPtr) {
+                    return false;
+                }
                 if (!corners) {
                     firstPt = pts;
                 } else {
