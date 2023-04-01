956b26800e36df8f22521dafb20ee0e42285a913
Lee Salzman <lsalzman@gmail.com>
2023-04-01 14:31:57 -0400

    Bug 1294455 - disable slow debug validation in Skia.

--- include/config/SkUserConfig.h
+++ include/config/SkUserConfig.h
@@ -156,6 +156,8 @@
 
 #define SK_DISABLE_LEGACY_PNG_WRITEBUFFER
 
+#define SK_DISABLE_SLOW_DEBUG_VALIDATION 1
+
 #ifndef MOZ_IMPLICIT
 #  ifdef MOZ_CLANG_PLUGIN
 #    define MOZ_IMPLICIT __attribute__((annotate("moz_implicit")))
