709a18f1b6dcecfe977b5d78dc20f4d70b1125f6
Lee Salzman <lsalzman@gmail.com>
2023-04-01 15:16:33 -0400

    Bug 1321031 - Preserve variations settings when creating a new CTFont in SkFontHost_mac.
    Bug 1331683 - Don't attempt to use any Core Text and Core Graphics variation-font APIs on pre-Sierra systems
    
    Bug 1455569 - Handle variation settings of system-installed variation fonts when creating CTFont from CGFont on High Sierra
    
    Bug 1454094 - Explicitly copy font variation settings from CGFont when creating a CTFont only on macOS Sierra; on HighSierra, rely on Core Text to automatically p
    
    Bug 1267180 - Don't draw emojis as paths when they are too big.
    
    Bug 1269247 - check that SkPaint has a typeface before using it
    
    Bug 1531867 - support light and dark subpixel AA masks in Skia on mac
    
    Bug 1542738 - disable Mac contrast adjustment in Skia
    
    Bug 1591432 - avoid creating paths for color fonts on macOS
    
    Bug 1672899 - Avoid CGFonts in ctfont_create_exact_copy when possible
    
    Bug 1673051 - Remove OnYosemiteOrLater(), OnElCapitanOrLater(), and OnSierraOrLater()
    
    Bug 1747272 - Apply current color when rasterizing glyphs from color fonts in Skia using Core Text
    
    Bug 1858347 - Fix instantiation of optically-sized system font in Skia on recent macOS versions
    
    Bug 1882947 - Adjust SkTypeface_mac_ct.cpp after bug 1858347.

--- src/core/SkStrikeSpec.cpp
+++ src/core/SkStrikeSpec.cpp
@@ -112,6 +112,11 @@ bool SkStrikeSpec::ShouldDrawAsPath(
         return true;
     }
 
+    // Glyphs like Emojis can't be rendered as a path.
+    if (font.getTypeface() && font.getTypeface()->hasColorGlyphs()) {
+      return false;
+    }
+
     SkMatrix textMatrix = SkFontPriv::MakeTextMatrix(font);
     textMatrix.postConcat(viewMatrix);
 
--- src/utils/mac/SkCTFontCreateExactCopy.cpp
+++ src/utils/mac/SkCTFontCreateExactCopy.cpp
@@ -13,6 +13,10 @@
 #include "src/ports/SkTypeface_mac_ct.h"
 #include "src/utils/mac/SkUniqueCFRef.h"
 
+#if defined(MOZ_SKIA) && defined(XP_MACOSX)
+#include "nsCocoaFeatures.h"
+#endif
+
 // In macOS 10.12 and later any variation on the CGFont which has default axis value will be
 // dropped when creating the CTFont. Unfortunately, in macOS 10.15 the priority of setting
 // the optical size (and opsz variation) is
@@ -49,6 +53,9 @@ SkUniqueCFRef<CTFontRef> SkCTFontCreateExactCopy(CTFontRef baseFont, CGFloat tex
 
     if (opszVariation.isSet) {
         add_opsz_attr(attr.get(), opszVariation.value);
+#ifdef MOZ_SKIA
+    }
+#else
     } else {
         // On (at least) 10.10 though 10.14 the default system font was SFNSText/SFNSDisplay.
         // The CTFont is backed by both; optical size < 20 means SFNSText else SFNSDisplay.
@@ -72,8 +79,102 @@ SkUniqueCFRef<CTFontRef> SkCTFontCreateExactCopy(CTFontRef baseFont, CGFloat tex
         add_opsz_attr(attr.get(), opsz_val);
     }
     add_notrak_attr(attr.get());
+#endif
+
+    // To figure out if a font is installed locally or used from a @font-face
+    // resource, we check whether its descriptor can provide a URL. This will
+    // be present for installed fonts, but not for those activated from an
+    // in-memory resource.
+    auto IsInstalledFont = [](CTFontRef aFont) {
+        CTFontDescriptorRef desc = CTFontCopyFontDescriptor(aFont);
+        CFTypeRef attr = CTFontDescriptorCopyAttribute(desc, kCTFontURLAttribute);
+        CFRelease(desc);
+        bool result = false;
+        if (attr) {
+            result = true;
+            CFRelease(attr);
+        }
+        return result;
+    };
+
+    SkUniqueCFRef<CGFontRef> baseCGFont;
+
+    // If we have a system font we need to use the CGFont APIs to avoid having the
+    // underlying font change for us when using CTFontCreateCopyWithAttributes.
+    CFDictionaryRef variations = nullptr;
+    if (IsInstalledFont(baseFont)) {
+        baseCGFont.reset(CTFontCopyGraphicsFont(baseFont, nullptr));
+
+        // The last parameter (CTFontDescriptorRef attributes) *must* be nullptr.
+        // If non-nullptr then with fonts with variation axes, the copy will fail in
+        // CGFontVariationFromDictCallback when it assumes kCGFontVariationAxisName is CFNumberRef
+        // which it quite obviously is not.
+
+        // Because we cannot setup the CTFont descriptor to match, the same restriction applies here
+        // as other uses of CTFontCreateWithGraphicsFont which is that such CTFonts should not escape
+        // the scaler context, since they aren't 'normal'.
+
+        // Avoid calling potentially buggy variation APIs on pre-Sierra macOS
+        // versions (see bug 1331683).
+        //
+        // And on HighSierra, CTFontCreateWithGraphicsFont properly carries over
+        // variation settings from the CGFont to CTFont, so we don't need to do
+        // the extra work here -- and this seems to avoid Core Text crashiness
+        // seen in bug 1454094.
+        //
+        // However, for installed fonts it seems we DO need to copy the variations
+        // explicitly even on 10.13, otherwise fonts fail to render (as in bug
+        // 1455494) when non-default values are used. Fortunately, the crash
+        // mentioned above occurs with data fonts, not (AFAICT) with system-
+        // installed fonts.
+        //
+        // So we only need to do this "the hard way" on Sierra, and for installed
+        // fonts on HighSierra+; otherwise, just let the standard CTFont function
+        // do its thing.
+        //
+        // NOTE in case this ever needs further adjustment: there is similar logic
+        // in four places in the tree (sadly):
+        //    CreateCTFontFromCGFontWithVariations in gfxMacFont.cpp
+        //    CreateCTFontFromCGFontWithVariations in ScaledFontMac.cpp
+        //    CreateCTFontFromCGFontWithVariations in cairo-quartz-font.c
+        //    ctfont_create_exact_copy in SkFontHost_mac.cpp
+
+        // Not UniqueCFRef<> because CGFontCopyVariations can return null!
+        variations = CGFontCopyVariations(baseCGFont.get());
+        if (variations) {
+            CFDictionarySetValue(attr.get(), kCTFontVariationAttribute, variations);
+        }
+    }
 
     SkUniqueCFRef<CTFontDescriptorRef> desc(CTFontDescriptorCreateWithAttributes(attr.get()));
+
+    if (baseCGFont.get()) {
+        auto ctFont = SkUniqueCFRef<CTFontRef>(
+            CTFontCreateWithGraphicsFont(baseCGFont.get(), textSize, nullptr, desc.get()));
+        if (variations) {
+#if defined(MOZ_SKIA) && defined(XP_MACOSX)
+            if (nsCocoaFeatures::OnVenturaOrLater()) {
+                // On recent macOS versions, CTFontCreateWithGraphicsFont fails to apply
+                // the variations from the descriptor, so to get the correct values we use
+                // CTFontCreateCopyWithAttributes to re-apply them to the new instance.
+                // (We don't do this on older versions to minimize the risk of regressing
+                // something that has been working OK in the past.)
+                SkUniqueCFRef<CFDictionaryRef> attrs(CFDictionaryCreate(
+                    nullptr, (const void**)&kCTFontVariationAttribute,
+                    (const void**)&variations, 1, &kCFTypeDictionaryKeyCallBacks,
+                    &kCFTypeDictionaryValueCallBacks));
+                // Get the original descriptor from the CTFont, then add the variations
+                // attribute to it.
+                SkUniqueCFRef<CTFontDescriptorRef> desc(CTFontCopyFontDescriptor(ctFont.get()));
+                desc.reset(CTFontDescriptorCreateCopyWithAttributes(desc.get(), attrs.get()));
+                // Return a copy of the font that has the variations added.
+                ctFont.reset(CTFontCreateCopyWithAttributes(ctFont.get(), 0.0, nullptr, desc.get()));
+            }
+#endif
+            CFRelease(variations);
+        }
+        return ctFont;
+    }
 
     return SkUniqueCFRef<CTFontRef>(
             CTFontCreateCopyWithAttributes(baseFont, textSize, nullptr, desc.get()));
